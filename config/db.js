1571c600fd452c43033a93e4eecada358b73b2f3
Altamash Ahmad <altamash@yourekai.com>
2024-12-29 11:59:44 +0530

    final

--- config/db.js
+++ config/db.js
@@ -9,29 +9,4 @@ const pool = new Pool({
     }
 });
 
-// Simplified error handling
-pool.on('error', (err) => {
-    console.error('Unexpected error on idle client', err);
-    process.exit(-1);
-});
-
-console.log('Attempting to connect to database...');
-console.log('Connection string:', process.env.DATABASE_URL);
-
-// Test the connection
-pool.connect((err, client, release) => {
-    if (err) {
-        console.error('Error acquiring client', err.stack);
-        return;
-    }
-    client.query('SELECT NOW()', (err, result) => {
-        release();
-        if (err) {
-            console.error('Error executing query', err.stack);
-            return;
-        }
-        console.log('Database connected successfully at:', result.rows[0].now);
-    });
-});
-
 module.exports = pool; 
